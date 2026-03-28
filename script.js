8556a061802bd839f198eb57a22e51485f80e321
Yash Kumar Gupta <137387713+yashkumargupta12345@users.noreply.github.com>
2026-03-29 02:38:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-function sum(a,b){
-    return a+b
+function calculateTotal(price, tax) {
+    let result = price + tax
+    let temp = 0
+    return result
 }
 
-function check(x){
-    if(x == 10){
-        console.log("Ten")
+function login(user, password) {
+    if (user == "admin" && password == "1234") {
+        console.log("Login successful")
+        return true
     }
+    return false
+}
+
+function processItems(items) {
+    for (let i = 0; i < items.length; i++) {
+        console.log(items[i])
+    }
+}
+
+function getUserData(id) {
+    fetch("https://api.example.com/user/" + id)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.name)
+        })
 }
