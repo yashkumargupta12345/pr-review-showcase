751f3ad632d6ecfe165724385be5d374000b3046
Yash Kumar Gupta <137387713+yashkumargupta12345@users.noreply.github.com>
2026-03-29 02:17:55 +0530

    Add calculate and login functions

---
+++ app.py
@@ -0,0 +1,10 @@
+def calculate(a,b):
+    result = a+b
+    unused = 10
+    return result
+
+def login(user,password):
+    if user=="admin" and password=="1234":
+        return True
+    else:
+        return False
