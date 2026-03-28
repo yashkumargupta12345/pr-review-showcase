74b94ea6b294618508e8e471fff6bf4a60193345
Yash Kumar Gupta <137387713+yashkumargupta12345@users.noreply.github.com>
2026-03-29 03:11:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,1 @@
-def calculate(a,b):
-    result = a+b
-    temp = 100
-    return result
 
-def login(user,password):
-    if user=="admin" and password=="1234":
-        print("Login success")
-        return True
-    else:
-        return False
-
-def process_data(data):
-    for i in range(len(data)):
-        print(data[i])
